701b5aba240ae2c1dc5d70d164153c1a13effa48
claremathile <claremathile@gmail.com>
2023-09-18 13:36:14 -0500

    Code Update

---
+++ js/index.js
@@ -0,0 +1,48 @@
+console.log("Your index.js file is loaded correctly!");
+
+
+$(".resumeButton").hover(
+    function() {
+        $(".resumeButton").css("background-color", "#A6BF49");
+},
+    function() {
+        $(".resumeButton").css("background-color", "#273859");
+    }
+);
+
+$(".projectButton").hover(
+    function() {
+        $(".projectButton").css("background-color", "#F2D8CE");
+},
+    function() {
+        $(".projectButton").css("background-color", "#273859");
+    }
+);
+
+$(".projectButton").hover(
+    function() {
+        $(".projectButton").css("color", "#273859");
+},
+    function() {
+        $(".projectButton").css("color", "white");
+    }
+);
+
+
+$(".projectButton2").hover(
+    function() {
+        $(".projectButton2").css("background-color", "#F2D8CE");
+},
+    function() {
+        $(".projectButton2").css("background-color", "#273859");
+    }
+);
+
+$(".projectButton2").hover(
+    function() {
+        $(".projectButton2").css("color", "#273859");
+},
+    function() {
+        $(".projectButton2").css("color", "white");
+    }
+);
